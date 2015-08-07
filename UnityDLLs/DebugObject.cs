07a8f3ddd39a7cc1de760be3e7c35bcd5cae75f4
rodgerwatercompany1 <rebornyschen@gmail.com>
2015-08-07 17:27:02 +0800

    Create

---
+++ UnityDLLs/DebugObject.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Net.Sockets;
+
+namespace Rodger
+{
+    public class DebugObject : MonoBehaviour
+    {
+
+        private TcpClient tcpClient;
+
+        public string ServerIP;        
+
+        void Awake()
+        {
+            if (!string.IsNullOrEmpty(ServerIP))
+                Global.SetupDebug(this, ServerIP);
+            else
+            {
+                Global.ReleaseDebugObj();
+
+                Destroy(gameObject);
+            }
+        }
+
+        public void Setup(string serverip, string logname)
+        {
+            try
+            {
+                //string IP = "192.168.152.205";
+                int port = 13000;
+
+                tcpClient = new TcpClient(serverip, port);
+
+                if (tcpClient.Connected)
+                {
+                    //StartCoroutine(Read());
+
+                    WriteToLServer("Login Username:" + logname);
+
+                }
+            }
+            catch (Exception EX)
+            {
+                Debug.Log("Connect Exception : " + EX);
+
+                Global.ReleaseDebugObj();
+
+                Destroy(gameObject);
+            }
+
+        }
+
+
+        public void print(string str)
+        {
+            WriteToLServer(str + "\n");
+        }
+        void WriteToLServer(string message)
+        {
+
+            if (tcpClient.Connected)
+            {
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = tcpClient.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
---
+++ UnityDLLs/RodgerGlobal.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+using System.Collections.Generic;
+
+namespace Rodger
+{
+
+    public delegate void VOIDCB();
+    public delegate void VOIDintCB(int value);
+
+    public class Global
+    {
+        static DebugObject DebugObj;
+
+        static public void SetupDebug(DebugObject obj, string serverip)
+        {
+            DebugObj = obj;
+            string logname = SystemInfo.graphicsDeviceName + " " + SystemInfo.deviceUniqueIdentifier;
+            DebugObj.Setup(serverip, logname);
+        }
+
+        static public void ReleaseDebugObj()
+        {
+            DebugObj = null;
+        }
+
+        static public void print(string str)
+        {
+            if (DebugObj != null)
+            {
+                Debug.Log("use logserver " + str);
+                DebugObj.print(str);
+            }
+        }
+
+    }
+}
